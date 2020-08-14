f85afdd4dc2c2d7bd056e84b98b2ab2096c2e823
daniilperestoronin <perestoronin.daniil@gmail.com>
2020-08-14 22:50:49 +0300

    Update auth_server

--- server/server/auth_server.go
+++ server/server/auth_server.go
@@ -17,139 +17,63 @@ import (
 	goauth "golang.org/x/oauth2"
 )
 
-// fosite requires four parameters for the server to get up and running:
-// 1. config - for any enforcement you may desire, you can do this using `compose.Config`. You like PKCE, enforce it!
-// 2. store - no auth service is generally useful unless it can remember clients and users.
-//    fosite is incredibly composable, and the store parameter enables you to build and BYODb (Bring Your Own Database)
-// 3. secret - required for code, access and refresh token generation.
-// 4. privateKey - required for id/jwt token generation.
-var (
-	// Check the api documentation of `compose.Config` for further configuration options.
-	config = &compose.Config{
-		AccessTokenLifespan: time.Minute * 30,
-	}
-
-	// This is the example storage that contains:
-	// * an OAuth2 Client with id "my-client" and secret "foobar" capable of all oauth2 and open id connect grant and response types.
-	// * a User for the resource owner password credentials grant type with username "peter" and password "secret".
-	//
-	// You will most likely replace this with your own logic once you set up a real world application.
-	store = storage.NewExampleStore()
-
-	// This secret is used to sign authorize codes, access and refresh tokens.
-	// It has to be 32-bytes long for HMAC signing. This requirement can be configured via `compose.Config` above.
-	// In order to generate secure keys, the best thing to do is use crypto/rand:
-	//
-	// ```
-	// package main
-	//
-	// import (
-	//	"crypto/rand"
-	//	"encoding/hex"
-	//	"fmt"
-	// )
-	//
-	// func main() {
-	//	var secret = make([]byte, 32)
-	//	_, err := rand.Read(secret)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	// }
-	// ```
-	//
-	// If you require this to key to be stable, for example, when running multiple fosite servers, you can generate the
-	// 32byte random key as above and push it out to a base64 encoded string.
-	// This can then be injected and decoded as the `var secret []byte` on server start.
-	secret = []byte("some-cool-secret-that-is-32bytes")
-
-	// privateKey is used to sign JWT tokens. The default strategy uses RS256 (RSA Signature with SHA-256)
-	privateKey, _ = rsa.GenerateKey(rand.Reader, 2048)
-)
-
-// Build a fosite instance with all OAuth2 and OpenID Connect handlers enabled, plugging in our configurations as specified above.
-var oauth2 = compose.ComposeAllEnabled(config, store, secret, privateKey)
-
-// A session is passed from the `/auth` to the `/token` endpoint. You probably want to store data like: "Who made the request",
-// "What organization does that person belong to" and so on.
-// For our use case, the session will meet the requirements imposed by JWT access tokens, HMAC access tokens and OpenID Connect
-// ID Tokens plus a custom field
-
-// newSession is a helper function for creating a new session. This may look like a lot of code but since we are
-// setting up multiple strategies it is a bit longer.
-// Usually, you could do:
-//
-//  session = new(fosite.DefaultSession)
-func newSession(user string) *openid.DefaultSession {
-	return &openid.DefaultSession{
-		Claims: &jwt.IDTokenClaims{
-			Issuer:      "https://fosite.my-application.com",
-			Subject:     user,
-			Audience:    []string{"https://my-client.my-application.com"},
-			ExpiresAt:   time.Now().Add(time.Hour * 6),
-			IssuedAt:    time.Now(),
-			RequestedAt: time.Now(),
-			AuthTime:    time.Now(),
-		},
-		Headers: &jwt.Headers{
-			Extra: make(map[string]interface{}),
-		},
-	}
-}
-
-// A valid oauth2 client (check the store) that additionally requests an OpenID Connect id token
-var clientConf = goauth.Config{
-	ClientID:     "my-client",
-	ClientSecret: "foobar",
-	RedirectURL:  "http://localhost:3846/callback",
-	Scopes:       []string{"photos", "openid", "offline"},
-	Endpoint: goauth.Endpoint{
-		TokenURL: "http://localhost:3846/oauth2/token",
-		AuthURL:  "http://localhost:3846/oauth2/auth",
-	},
-}
-
 // OAuth2Server serve
 type OAuth2Server struct {
+	Configuration *compose.Config
+	Storage       *storage.MemoryStore
+	Secret        []byte
+	PrivateKey    *rsa.PrivateKey
+	oauth         fosite.OAuth2Provider
+}
+
+func (oauthServe *OAuth2Server) buildServer() {
+	if oauthServe.Configuration == nil {
+		oauthServe.Configuration = &compose.Config{
+			AccessTokenLifespan: time.Minute * 30,
+		}
+	}
+	if oauthServe.Storage == nil {
+		oauthServe.Storage = storage.NewExampleStore()
+	}
+	if oauthServe.Secret == nil {
+		oauthServe.Secret = []byte("some-cool-secret-that-is-32bytes")
+	}
+	if oauthServe.PrivateKey == nil {
+		oauthServe.PrivateKey, _ = rsa.GenerateKey(rand.Reader, 2048)
+	}
+
+	oauthServe.oauth = compose.ComposeAllEnabled(
+		oauthServe.Configuration,
+		oauthServe.Storage,
+		oauthServe.Secret,
+		oauthServe.PrivateKey)
 }
 
 // Serve ff
 func (oauthServe *OAuth2Server) Serve() error {
-	// Set up oauth2 endpoints. You could also use gorilla/mux or any other router.
-	http.HandleFunc("/oauth2/auth", authEndpoint)
-	http.HandleFunc("/oauth2/token", tokenEndpoint)
+	oauthServe.buildServer()
 
-	// revoke tokens
-	http.HandleFunc("/oauth2/revoke", revokeEndpoint)
-	http.HandleFunc("/oauth2/introspect", introspectionEndpoint)
-
-	http.HandleFunc("/login", OwnerHandler(clientConf))
-
+	http.HandleFunc("/oauth2/auth", oauthServe.authEndpoint)
+	http.HandleFunc("/oauth2/token", oauthServe.tokenEndpoint)
+	http.HandleFunc("/oauth2/revoke", oauthServe.revokeEndpoint)
+	http.HandleFunc("/oauth2/introspect", oauthServe.introspectionEndpoint)
+	http.HandleFunc("/login", oauthServe.ownerHandler(clientConf))
 	log.Print("Starting OAuth2 server")
 	return http.ListenAndServe(":3846", nil)
 }
 
-func authEndpoint(rw http.ResponseWriter, req *http.Request) {
-	// This context will be passed to all methods.
+func (oauthServe *OAuth2Server) authEndpoint(rw http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
-
-	// Let's create an AuthorizeRequest object!
-	// It will analyze the request and extract important information like scopes, response type and others.
-	ar, err := oauth2.NewAuthorizeRequest(ctx, req)
+	ar, err := oauthServe.oauth.NewAuthorizeRequest(ctx, req)
 	if err != nil {
 		log.Printf("Error occurred in NewAuthorizeRequest: %+v", err)
-		oauth2.WriteAuthorizeError(rw, ar, err)
+		oauthServe.oauth.WriteAuthorizeError(rw, ar, err)
 		return
 	}
-	// You have now access to authorizeRequest, Code ResponseTypes, Scopes ...
-
 	var requestedScopes string
 	for _, this := range ar.GetRequestedScopes() {
 		requestedScopes += fmt.Sprintf(`<li><input type="checkbox" name="scopes" value="%s">%s</li>`, this, this)
 	}
-
-	// Normally, this would be the place where you would check if the user is logged in and gives his consent.
-	// We're simplifying things and just checking if the request includes a valid username and password
 	req.ParseForm()
 	if req.PostForm.Get("username") != "peter" {
 		rw.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -167,76 +91,28 @@ func authEndpoint(rw http.ResponseWriter, req *http.Request) {
 		`, requestedScopes)))
 		return
 	}
-
-	// let's see what scopes the user gave consent to
 	for _, scope := range req.PostForm["scopes"] {
 		ar.GrantScope(scope)
 	}
-
-	// Now that the user is authorized, we set up a session:
 	mySessionData := newSession("peter")
-
-	// When using the HMACSHA strategy you must use something that implements the HMACSessionContainer.
-	// It brings you the power of overriding the default values.
-	//
-	// mySessionData.HMACSession = &strategy.HMACSession{
-	//	AccessTokenExpiry: time.Now().Add(time.Day),
-	//	AuthorizeCodeExpiry: time.Now().Add(time.Day),
-	// }
-	//
-
-	// If you're using the JWT strategy, there's currently no distinction between access token and authorize code claims.
-	// Therefore, you both access token and authorize code will have the same "exp" claim. If this is something you
-	// need let us know on github.
-	//
-	// mySessionData.JWTClaims.ExpiresAt = time.Now().Add(time.Day)
-
-	// It's also wise to check the requested scopes, e.g.:
-	// if ar.GetRequestedScopes().Has("admin") {
-	//     http.Error(rw, "you're not allowed to do that", http.StatusForbidden)
-	//     return
-	// }
-
-	// Now we need to get a response. This is the place where the AuthorizeEndpointHandlers kick in and start processing the request.
-	// NewAuthorizeResponse is capable of running multiple response type handlers which in turn enables this library
-	// to support open id connect.
-	response, err := oauth2.NewAuthorizeResponse(ctx, ar, mySessionData)
-
-	// Catch any errors, e.g.:
-	// * unknown client
-	// * invalid redirect
-	// * ...
+	response, err := oauthServe.oauth.NewAuthorizeResponse(ctx, ar, mySessionData)
 	if err != nil {
 		log.Printf("Error occurred in NewAuthorizeResponse: %+v", err)
-		oauth2.WriteAuthorizeError(rw, ar, err)
+		oauthServe.oauth.WriteAuthorizeError(rw, ar, err)
 		return
 	}
-
-	// Last but not least, send the response!
-	oauth2.WriteAuthorizeResponse(rw, ar, response)
+	oauthServe.oauth.WriteAuthorizeResponse(rw, ar, response)
 }
 
-func tokenEndpoint(rw http.ResponseWriter, req *http.Request) {
-	// This context will be passed to all methods.
+func (oauthServe *OAuth2Server) tokenEndpoint(rw http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
-
-	// Create an empty session object which will be passed to the request handlers
 	mySessionData := newSession("")
-
-	// This will create an access request object and iterate through the registered TokenEndpointHandlers to validate the request.
-	accessRequest, err := oauth2.NewAccessRequest(ctx, req, mySessionData)
-
-	// Catch any errors, e.g.:
-	// * unknown client
-	// * invalid redirect
-	// * ...
+	accessRequest, err := oauthServe.oauth.NewAccessRequest(ctx, req, mySessionData)
 	if err != nil {
 		log.Printf("Error occurred in NewAccessRequest: %+v", err)
-		oauth2.WriteAccessError(rw, accessRequest, err)
+		oauthServe.oauth.WriteAccessError(rw, accessRequest, err)
 		return
 	}
-
-	// If this is a client_credentials grant, grant all scopes the client is allowed to perform.
 	if accessRequest.GetGrantTypes().Exact("client_credentials") {
 		for _, scope := range accessRequest.GetRequestedScopes() {
 			if fosite.HierarchicScopeStrategy(accessRequest.GetClient().GetScopes(), scope) {
@@ -244,48 +120,35 @@ func tokenEndpoint(rw http.ResponseWriter, req *http.Request) {
 			}
 		}
 	}
-
-	// Next we create a response for the access request. Again, we iterate through the TokenEndpointHandlers
-	// and aggregate the result in response.
-	response, err := oauth2.NewAccessResponse(ctx, accessRequest)
+	response, err := oauthServe.oauth.NewAccessResponse(ctx, accessRequest)
 	if err != nil {
 		log.Printf("Error occurred in NewAccessResponse: %+v", err)
-		oauth2.WriteAccessError(rw, accessRequest, err)
+		oauthServe.oauth.WriteAccessError(rw, accessRequest, err)
 		return
 	}
-
-	// All done, send the response.
-	oauth2.WriteAccessResponse(rw, accessRequest, response)
-
-	// The client now has a valid access token
+	oauthServe.oauth.WriteAccessResponse(rw, accessRequest, response)
 }
 
-func introspectionEndpoint(rw http.ResponseWriter, req *http.Request) {
+func (oauthServe *OAuth2Server) introspectionEndpoint(rw http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
 	mySessionData := newSession("")
-	ir, err := oauth2.NewIntrospectionRequest(ctx, req, mySessionData)
+	ir, err := oauthServe.oauth.NewIntrospectionRequest(ctx, req, mySessionData)
 	if err != nil {
 		log.Printf("Error occurred in NewIntrospectionRequest: %+v", err)
-		oauth2.WriteIntrospectionError(rw, err)
+		oauthServe.oauth.WriteIntrospectionError(rw, err)
 		return
 	}
-
-	oauth2.WriteIntrospectionResponse(rw, ir)
+	oauthServe.oauth.WriteIntrospectionResponse(rw, ir)
 }
 
-func revokeEndpoint(rw http.ResponseWriter, req *http.Request) {
-	// This context will be passed to all methods.
+func (oauthServe *OAuth2Server) revokeEndpoint(rw http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
-
-	// This will accept the token revocation request and validate various parameters.
-	err := oauth2.NewRevocationRequest(ctx, req)
-
-	// All done, send the response.
-	oauth2.WriteRevocationResponse(rw, err)
+	err := oauthServe.oauth.NewRevocationRequest(ctx, req)
+	oauthServe.oauth.WriteRevocationResponse(rw, err)
 }
 
 //OwnerHandler hh
-func OwnerHandler(c goauth.Config) func(rw http.ResponseWriter, req *http.Request) {
+func (oauthServe *OAuth2Server) ownerHandler(c goauth.Config) func(rw http.ResponseWriter, req *http.Request) {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		rw.Write([]byte("<h1>Login</h1>"))
 		req.ParseForm()
@@ -305,7 +168,6 @@ func OwnerHandler(c goauth.Config) func(rw http.ResponseWriter, req *http.Reques
 		</form>`))
 			return
 		}
-
 		token, err := c.PasswordCredentialsToken(context.Background(), req.Form.Get("username"), req.Form.Get("password"))
 		if err != nil {
 			rw.Write([]byte(fmt.Sprintf(`<p>I tried to get a token but received an error: %s</p>`, err.Error())))
@@ -315,4 +177,33 @@ func OwnerHandler(c goauth.Config) func(rw http.ResponseWriter, req *http.Reques
 		rw.Write([]byte(fmt.Sprintf(`<p>Awesome, you just received an access token!<br><br>%s<br><br><strong>more info:</strong><br><br>%s</p>`, token.AccessToken, token)))
 		rw.Write([]byte(`<p><a href="/">Go back</a></p>`))
 	}
+}
+
+// A valid oauth2 client (check the store) that additionally requests an OpenID Connect id token
+func newSession(user string) *openid.DefaultSession {
+	return &openid.DefaultSession{
+		Claims: &jwt.IDTokenClaims{
+			Issuer:      "https://fosite.my-application.com",
+			Subject:     user,
+			Audience:    []string{"https://my-client.my-application.com"},
+			ExpiresAt:   time.Now().Add(time.Hour * 6),
+			IssuedAt:    time.Now(),
+			RequestedAt: time.Now(),
+			AuthTime:    time.Now(),
+		},
+		Headers: &jwt.Headers{
+			Extra: make(map[string]interface{}),
+		},
+	}
+}
+
+var clientConf = goauth.Config{
+	ClientID:     "my-client",
+	ClientSecret: "foobar",
+	RedirectURL:  "http://localhost:3846/callback",
+	Scopes:       []string{"photos", "openid", "offline"},
+	Endpoint: goauth.Endpoint{
+		TokenURL: "http://localhost:3846/oauth2/token",
+		AuthURL:  "http://localhost:3846/oauth2/auth",
+	},
 }
